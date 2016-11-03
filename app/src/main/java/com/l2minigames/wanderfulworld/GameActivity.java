6059c535ce7b92fe64ed3f52d04ac49786a49ba2
FUEDU\llumea <lennilarsson@hotmail.com>
2016-11-03 16:04:04 +0100

    3 nov Google Maps

--- app/src/main/java/com/l2minigames/wanderfulworld/GameActivity.java
+++ app/src/main/java/com/l2minigames/wanderfulworld/GameActivity.java
@@ -100,9 +100,10 @@ public class GameActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(GameActivity.this, MapsActivity.class);
+                startActivity(intent);
+               /// Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                ///        .setAction("Action", null).show();
             }
         });
 
