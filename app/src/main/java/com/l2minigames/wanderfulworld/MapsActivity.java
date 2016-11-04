900da1d0cd5d5963309ecc23a39b210cee3b74ae
FUEDU\llumea <lennilarsson@hotmail.com>
2016-11-04 15:00:11 +0100

    4 november - Location, tilt och marker i Google Maps

--- app/src/main/java/com/l2minigames/wanderfulworld/MapsActivity.java
+++ app/src/main/java/com/l2minigames/wanderfulworld/MapsActivity.java
@@ -1,23 +1,65 @@
 package com.l2minigames.wanderfulworld;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.Manifest;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
 
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    TextView mPositionLatitude;
+    TextView mPositionLongitude;
+    LocationRequest locationRequest;
+    double myPositionLatitude;
+    double myPositionLongitude;
+    final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000); //10 sek mellan requests
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        mPositionLatitude = (TextView)findViewById(R.id.position_latitude);
+        mPositionLongitude = (TextView)findViewById(R.id.position_longitude);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -37,10 +79,132 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+       /// mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
 
+
+        ///mMap.setMaxZoomPreference(20);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng mPosition = new LatLng(myPositionLatitude, myPositionLongitude);
+        mMap.addMarker(new MarkerOptions().position(mPosition).title("MyMarker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mPosition));
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                new CameraPosition.Builder()
+                        .target(mPosition)
+                        .tilt(45)
+                        .zoom(16)
+                        .build()));
+
+        ///LatLng sydney = new LatLng(-34, 151);
+       /// mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+       /// mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i("Tag", "In OnConnected");
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        Log.i("Tag", "permissionCheck: " + permissionCheck);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+            // No explanation needed, we can request the permission.
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
+
+        }
+        else{
+            getUpDates();
+        }
+
+    }
+    public void getUpDates(){
+        try{
+
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,locationRequest, this);
+            /// mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        }catch (SecurityException e){
+
+        }
+        if (mLastLocation != null) {
+            mPositionLatitude.setText(String.valueOf(mLastLocation.getLatitude()));
+            mPositionLongitude.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    getUpDates();
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    Log.i("Tag","not permitted");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mMap.clear();
+        mMap.getUiSettings(). setZoomGesturesEnabled(false);
+        myPositionLatitude = location.getLatitude();
+        myPositionLongitude = location.getLongitude();
+        mPositionLatitude.setText(String.valueOf(location.getLatitude()));
+        mPositionLongitude.setText(String.valueOf(location.getLongitude()));
+        Log.d("TAG", "MyLatitude in onLocationChanged: "+location.getLatitude());
+        Log.d("TAG", "MyLongitude in onLocationChanged: "+location.getLongitude());
+        LatLng mPosition = new LatLng(myPositionLatitude, myPositionLongitude);
+        mMap.addMarker(new MarkerOptions().position(mPosition).title("MyMarker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(mPosition));
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                new CameraPosition.Builder()
+                        .target(mPosition)
+                        .tilt(45)
+                        .zoom(16)
+                        .build()));
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 }
